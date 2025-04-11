b9d2a9742441ca4d901d00eb8e9f3d99b00e9084
PeZunino <pedrozsantos@hotmail.com>
2025-04-11 18:24:07 -0300

    chore: next eslint config vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const nextConfig = {
-};
+const nextConfig = {eslint: {ignoreDuringBuilds: true,}};
 
 module.exports = withVanillaExtract(nextConfig);
