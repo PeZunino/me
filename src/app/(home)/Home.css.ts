4399fab0fbc133bfda3e4f26f827c89fef85ea07
PeZunino <pedrozsantos@hotmail.com>
2025-04-24 16:38:21 -0300

    feat(ui): add personal links and subtitle to Home header

--- src/app/(home)/Home.css.ts
+++ src/app/(home)/Home.css.ts
@@ -7,7 +7,13 @@ export const home = style({
 	margin:'0 auto',
 	display: 'flex',
 	padding: '0rem 3rem',
-	'@media': {'(max-width: 900px)': {flexDirection: 'column'}},
+	'@media': {
+		'(max-width: 900px)': {
+			flexDirection: 'column',
+			padding: '0rem 6rem',
+
+		}
+	},
 });
 
 
@@ -15,15 +21,58 @@ globalStyle(`${home} >*`,{paddingTop:'6rem',});
 
 export const home__header = style({
 	flex:'1',
-	height: 'fit-content',
+	height: '100vh',
+
 	position: 'sticky',
 	top: '0',
+
 	'@media': {'(max-width: 900px)': {	position: 'inherit'}},
 
 });
 
-
 globalStyle(`${home__header} h1`,{width: 'max-content',});
+
+globalStyle(`${home__header} h2`,{
+	marginTop: '0.75rem',
+	marginBottom: '1rem',
+	'@media': {
+		'(max-width: 900px)': {
+			marginTop: '2rem',
+			marginBottom: '3rem',
+		}
+	},
+
+});
+
+globalStyle(`${home__header} P`,{
+	width: '60%',
+	marginBottom: '3rem',
+	color: vars.color.gray,
+	'@media': {'(max-width: 900px)': { width: '90%',}},
+
+});
+
+
+globalStyle(`${home__header} div`,{
+	display: 'flex',
+	gap:'1.25rem',
+	'@media': {
+		'(max-width: 900px)': {
+			marginTop:'3rem',
+			gap:'5rem',
+
+		}
+	},
+
+});
+
+globalStyle(`${home__header} svg`,{
+	color: vars.color.gray,
+	cursor:'pointer'
+});
+
+globalStyle(`${home__header} svg:hover`,{color: vars.color.white});
+
 
 globalStyle(`${home} main`,{
 	width: '60%',
@@ -35,13 +84,16 @@ export const home__presentation_text = style({
 	marginBottom:'2rem',
 	lineHeight: '1.625',
 	color:vars.color.gray,
-	'@media': {'(max-width: 900px)': {	marginBottom: '8rem'}},
+	'@media': {
+		'(max-width: 900px)': {
+			marginBottom: '8rem',
+			marginTop: '10rem'
+		}
+	},
 
 });
 
 globalStyle(`${home__presentation_text} p`,{display: 'inline',});
-
-globalStyle(`${home} main section`,{});
 
 export const home__resumeLink = style({':hover': {color: vars.color.yellow500}});
 
--- src/app/(home)/Home.page.tsx
+++ src/app/(home)/Home.page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { IoLogoGithub, IoLogoLinkedin, IoMdMail } from 'react-icons/io';
 import { MdOutlineArrowOutward } from 'react-icons/md';
 import { Card } from '@/components/Card/Card';
 import Reveal from '@/components/Reveal/Reveal';
@@ -29,7 +30,37 @@ export default function Home() {
 				<header className={home__header}>
 					<h1>Pedro Zunino</h1>
 					<h2>Desenvolvedor fullstack</h2>
+					<p>
+						Transformo ideias em aplicações robustas, escaláveis e integradas — rápidas, limpas e prontas para prod
+					</p>
 					<div>
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://github.com/PeZunino"
+							title="PeZunino"
+
+						>
+							<IoLogoGithub size={24} />
+						</a>
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="mailto:pedrozunino.santos@email.com"
+							title="pedrozunino.santos@email.com"
+
+						>
+							<IoMdMail size={24} />
+						</a>
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://www.linkedin.com/in/pedro-zunino-dos-santos-0880bb188?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+							title="Pedro Zunino dos Santos"
+
+						>
+							<IoLogoLinkedin size={24} />
+						</a>
 					</div>
 				</header>
 
