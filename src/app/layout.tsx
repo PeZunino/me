b27ba509c661a316baca129f4ec0726eca093cb9
PeZunino <pedrozsantos@hotmail.com>
2025-04-24 16:00:25 -0300

    feat(analytics): integrate Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css.ts';
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
-
 const manrope = Manrope({
 	subsets: [
 		'latin'
@@ -26,6 +26,7 @@ export default function RootLayout({ children, }: Readonly<{
 		<html lang="en">
 			<body className={manrope.className}>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
