5f200110c03923e3e713f052a5a55263ca9fa9f5
PeZunino <pedrozsantos@hotmail.com>
2026-02-26 23:23:27 -0300

    fix: force webpack on build to fix vanilla-extract turbopack incompatibility

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
 	eslint: {ignoreDuringBuilds: true,},
 	reactStrictMode: false,
+	turbopack: {} 
 };
 
 module.exports = withVanillaExtract(nextConfig);
